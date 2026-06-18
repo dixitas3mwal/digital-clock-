126c2fcd9471f6976158c5d607f0112e53086c2c
dixi <deekshitasemwal@gmail.com>
2026-06-18 11:20:13 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+function showTime(){
+
+```
+let time = new Date();
+
+let hours = time.getHours();
+let minutes = time.getMinutes();
+let seconds = time.getSeconds();
+
+if(hours < 10){
+    hours = "0" + hours;
+}
+
+if(minutes < 10){
+    minutes = "0" + minutes;
+}
+
+if(seconds < 10){
+    seconds = "0" + seconds;
+}
+
+document.getElementById("clock").innerHTML =
+hours + ":" + minutes + ":" + seconds;
+```
+
+}
+
+showTime();
+
+setInterval(showTime,1000);
